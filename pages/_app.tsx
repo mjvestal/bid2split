80d38f159bdee716489fa2b9d5baed4e8ed9afb4
Matt Vestal <mjvestal@gmail.com>
2022-05-05 21:11:40 -0700

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PVYLJPFPBV"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PVYLJPFPBV');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>);
 }
 
 export default MyApp
