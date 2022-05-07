f79a2597f514d8287c3e65213c373831cf8ceb89
Matt Vestal <mjvestal@gmail.com>
2022-05-07 13:27:13 -0700

    Fix vertical center layout on iOS safari

--- components/VerticalCenterLayout.tsx
+++ components/VerticalCenterLayout.tsx
@@ -8,8 +8,8 @@ export default function VerticalCenterLayout({
   className?: string,
 }) {
   return (
-    <div className={`flex min-h-screen h-screen-ios min-h-screen-ios flex-col items-center py-20 ${className}`}>
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-8">
+    <div className={`flex min-h-screen h-screen-ios min-h-screen-ios flex-col items-center ${className}`}>
+      <main className="flex w-full flex-1 flex-col items-center justify-center py-20 px-8">
         <div className="flex w-full md:max-w-3xl flex-col">
           {children}
         </div>
